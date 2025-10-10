91bffd3969ddb2da3834db7773932369b24a96c9
RikzProgs <rikzafaf@gmail.com>
2025-10-10 22:46:09 +0500

    opacity changes made

--- src/Projects.js
+++ src/Projects.js
@@ -4,21 +4,13 @@ import { Link } from "react-router-dom";
 import Project1 from "./assets/project0001.jpg";
 import Project2 from "./assets/project0002.jpg";
 import Project3 from "./assets/project0003.jpg";
-// import Project4 from "./assets/project4.jpg";
-// import Project5 from "./assets/project5.jpg";
-// import Project6 from "./assets/project6.jpg";
-// import Project7 from "./assets/project7.jpg";
-// import Project8 from "./assets/project8.jpg";
+
 
 const projects = [
   { id: 1, title: "Project 1", image: Project1, path: "/project1" },
   { id: 2, title: "Project 2", image: Project2, path: "/project2" },
   { id: 3, title: "Project 3", image: Project3, path: "/project3" },
-//   { id: 4, title: "Project 4", image: Project4, path: "/project4" },
-//   { id: 5, title: "Project 5", image: Project5, path: "/project5" },
-//   { id: 6, title: "Project 6", image: Project6, path: "/project6" },
-  // { id: 7, title: "Project 7", image: Project7, path: "/project7" },
-  // { id: 8, title: "Project 8", image: Project8, path: "/project8" },
+
 ];
 
 const Projects = () => {
@@ -38,7 +30,7 @@ const Projects = () => {
               style={{ backgroundImage: `url(${project.image})` }}
             >
               {/* Overlay */}
-              <div className="absolute inset-0 bg-black bg-opacity-40 lg:opacity-0 lg:group-hover:opacity-100 transition duration-500"></div>
+              <div className="absolute inset-0 bg-black lg:bg-opacity-40 lg:opacity-0 lg:group-hover:opacity-100 transition duration-500"></div>
 
               {/* Title */}
               <span className="absolute bottom-6 left-6 font-netflix text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white transition duration-500 lg:group-hover:scale-110 lg:group-hover:text-purple-900">
