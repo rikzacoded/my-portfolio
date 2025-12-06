d7f80b9c79d0c5289247813ff4f5f4c21e18c20a
RikzProgs <rikzafaf@gmail.com>
2025-12-06 14:52:50 +0500

    fixed the placement of toat

--- src/Toast.js
+++ src/Toast.js
@@ -1,8 +1,11 @@
-import React from "react";
+
 
 const Toast = ({ message }) => {
   return (
-    <div className="fixed top-5 right-5 z-[9999] bg-purple-600 text-white px-4 py-2 rounded-lg shadow-lg animate-slideIn">
+    <div
+      className="fixed top-5 left-1/2 transform -translate-x-1/2 
+                 bg-purple-600 text-white px-6 py-3 rounded-lg shadow-lg z-[9999]"
+    >
       {message}
     </div>
   );
